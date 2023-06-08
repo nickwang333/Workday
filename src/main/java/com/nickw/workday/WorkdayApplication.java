f3f49a701b4a92e8087a841ee7cecea74a972a82
Zhuofeng Wang <zw2922@nyu.edu>
2023-06-08 15:57:18 -0700

    Initializw

---
+++ src/main/java/com/nickw/workday/WorkdayApplication.java
@@ -0,0 +1,13 @@
+package com.nickw.workday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkdayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickw/workday/WorkdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickw.workday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
