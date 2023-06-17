bdef9f616fa63593476f39e1838fa01ffcd247bb
Zhuofeng Wang <zw2922@nyu.edu>
2023-06-16 17:25:51 -0700

    mail sender created

---
+++ mailserver/jmail/src/main/java/com/nickw/jmail/JmailApplication.java
@@ -0,0 +1,13 @@
+package com.nickw.jmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmailApplication.class, args);
+	}
+
+}
---
+++ mailserver/jmail/src/test/java/com/nickw/jmail/JmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickw.jmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
